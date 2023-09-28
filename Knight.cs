bb6c9ede18b62b9c2beeeb6f2f37903dcedb6db0
Klaus Hoehfurtner <HoehfurtnerK190028@sus.htl-grieskirchen.at>
2023-09-28 20:11:16 +0200

    Pferd fertig

--- Knight.cs
+++ Knight.cs
@@ -10,7 +10,28 @@ namespace ChessGame
     {
         public override bool CanMove(ChessMove move, Gamecharacter[,] board)
         {
-            throw new NotImplementedException();
+            int rowDiff = move.endposition.Row - move.startposition.Row;
+            int colDiff = move.endposition.Column - move.startposition.Column;
+            //Vergleichen negativ
+            rowDiff = (rowDiff < 0) ? rowDiff * -1 : rowDiff;
+            colDiff = (colDiff < 0) ? colDiff * -1 : colDiff;
+
+            if (rowDiff == 2 && colDiff == 1)
+            {
+                return true;
+            }else if(rowDiff == 1 && colDiff == 2) {
+                return true;
+            }else if(rowDiff == -2  && colDiff == -1) {
+                return true;
+            }
+            else if(rowDiff == -1 && colDiff == -2){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
         public override string ToString()
